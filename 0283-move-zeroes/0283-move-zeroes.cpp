9358099668e2fa28459ea41adddbcdc9ea170861
Uzair Jafri <uzairjafri07@gmail.com>
2026-07-10 10:25:38 +0530

    Time: 0 ms (100%), Space: 24 MB (19.2%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+        while(count < n)
+        {
+            nums[count++] = 0;
+        }
+    }
+};
